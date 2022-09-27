cd059262b8fce74c1abe928221815d575b92312d
David <parrydavid@live.co.uk>
2022-09-27 15:56:50 +0000

    changed 'post' to 'blog' for easier understanding

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ STATUS = (
 )
 
 
-class Post(models.Model):
+class Blog(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Blog
 
-# Create your views here.
+
+class BlogList(generic.ListView):
+    model = Blog
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog.html'
+    paginate_by = 4
