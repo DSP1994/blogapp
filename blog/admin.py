9de3c82763c8b142d03fd2f71647280e13142efe
David <parrydavid@live.co.uk>
2022-09-27 15:42:35 +0000

    created a CommentAdmin class, to allow comments to be seen/read by admin and approved where necessary

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -11,3 +11,14 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
 
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approved_comments']
+
+    def approved_comments(self, request, queryset):
+        queryset.update(active=True)
