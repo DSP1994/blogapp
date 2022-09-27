7ef277c9f16dc786d051021cf939bca285e781c0
David <parrydavid@live.co.uk>
2022-09-27 17:18:52 +0000

    error appearing; 'ReverseManyToOneDescriptor' object has no attribute 'filter', finding a way to fix this in views.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BlogList.as_view(), name='home'),
+    path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_details.html')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Blog
 
 
 class BlogList(generic.ListView):
     model = Blog
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Blog.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog.html'
     paginate_by = 4
+
+
+class BlogDetail(View):
+    
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Blog.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = Blog.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if Blog.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            'blog_details.html',
+            {
+                'post': post,
+                'comments': comments,
+                'liked': liked
+            },
+        )
+    
