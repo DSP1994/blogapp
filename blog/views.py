f49a2e737520aef3143025f470ceab05c7014e58
David <parrydavid@live.co.uk>
2022-09-28 11:48:28 +0000

    in views.py changed comment.post to comment.blog to fix current IntegrityError

--- blog/views.py
+++ blog/views.py
@@ -46,8 +46,8 @@ class BlogDetail(View):
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
-            comment = comment_form.save(commnit=False)
-            comment.post = post
+            comment = comment_form.save(commit=False)
+            comment.blog = post
             comment.save()
         else:
             comment_form = CommentForm()
