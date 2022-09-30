96239e2c4dd7349a4301db4df24748a4e3bee037
David <parrydavid@live.co.uk>
2022-09-30 13:55:14 +0000

    created 'about' class in models.py for pages

---
+++ pages/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
+class About(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="about_sect")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='about_placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
