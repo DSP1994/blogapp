296461b31bbe89eafb95b275472ef8fbee4ab8a0
David <parrydavid@live.co.uk>
2022-09-30 14:00:02 +0000

    created admin view of about section

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import About
+from django_summernote import SummernoteModelAdmin
 
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+    
 # Register your models here.
