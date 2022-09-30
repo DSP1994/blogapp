f8d6c0a521dffa18fce5c00ce17259b229cba4f0
David <parrydavid@live.co.uk>
2022-09-30 13:58:15 +0000

    updated urlpatern in urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
 from . import views
+from django.urls import path
+
 
 urlpatterns = [
     path('frontpage/', views.frontpage, name='frontpage'),
     path('contact/', views.contact_page, name='contact_page'),
-    path('about/', views.about_page, name='about_page'),
+    path('about/', views.AboutSect.as_view(), name='about/html'),
 ]
