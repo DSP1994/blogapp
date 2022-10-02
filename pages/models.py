21656bc533d1bb9e2bd68712b8ad5c7eaa03976b
David <parrydavid@live.co.uk>
2022-10-02 11:40:41 +0000

    tried modifying names of the contact page for it to load on the page, Error 405 appeared. Issue with the POST/GET model (I believe)

--- pages/models.py
+++ pages/models.py
@@ -12,7 +12,8 @@ STATUS = (
 class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="about_sect")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="about_sect")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='about_placeholder')
