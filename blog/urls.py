1dae84bfd82ce8863e907e7e7d0e7fdeb01e1b9f
David <parrydavid@live.co.uk>
2022-10-02 11:17:20 +0000

    created a urlpattern for the liked blog post

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('blog/', views.BlogList.as_view(), name='blog.html'),
-    path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_details.html')
+    path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_details.html'),
+    path('like/<slug:slug>', viewsBlogLike.as_view(), name='blog_like')
 ]
