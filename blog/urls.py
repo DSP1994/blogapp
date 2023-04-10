8312b1fb10c75299dc573edb8fd27c943b8c1194
David <parrydavid@live.co.uk>
2023-04-10 11:53:24 +0000

    created and commented out the url path links I will need for my adding, editing, and deleting of a post

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 
 urlpatterns = [
     path('blog/', views.BlogList.as_view(), name='blog'),
+    # path('post/<slug:slug>/', views.PostDetail.as_view(), name='open_post'),
+    # path('add_post/', views.AddPost.as_view(), name='add_post'),
+    # path('update/<slug:slug>', views.UpdateView.as_view(), name='edit_post'),
+    # path('delete/<slug:slug>', views.DeleteView.as_view(), name='delete_post'),
     path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_details.html'),
     path('like/<slug:slug>', views.BlogLike.as_view(), name='blog_like')
 ]
