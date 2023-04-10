800c8a7bdd970c1d5122d93f7a3632a99676197c
David <parrydavid@live.co.uk>
2023-04-10 12:07:10 +0000

    added url, save, title and ordering to class

--- blog/models.py
+++ blog/models.py
@@ -49,6 +49,18 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('open_post', kwargs={'slug': str(self.slug)})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(kwargs.pop('slug', self.slug))
+        super().save(*args, **kwargs)
 
 
 class Comment(models.Model):
