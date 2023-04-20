e623aedf0e320417eb3856539840d9520c7a67f0
David <parrydavid@live.co.uk>
2023-04-20 13:51:04 +0000

    moved profile url pattern above the slugs

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('blog/', views.BlogList.as_view(), name='blog'),
+    path('profile/', views.ProfilePage.as_view(), name='profile_page'),  
     path('post/', views.PostList.as_view(), name='post'),
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='open_post'),
     path('add_post/', views.AddPost.as_view(), name='add_post'),
@@ -11,6 +12,4 @@ urlpatterns = [
     path('delete/<slug:slug>', views.DeleteView.as_view(), name='delete_post'),
     path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_details.html'),
     path('like/<slug:slug>', views.BlogLike.as_view(), name='blog_like'),
-    path('profile/', views.ProfilePage.as_view(), name='profile_page'),
-
 ]
